920be8044beda98c9365fafccdc098f15b0d7351
Samarth Kumar <60310341+samarth762@users.noreply.github.com>
2022-06-30 13:33:20 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (45.59%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> s;
+        int val=0;
+        for(int i=0;i<jewels.size();i++){
+            s.insert(jewels[i]);
+        }
+        for(int i=0;i<stones.size();i++){
+            if(s.find(stones[i])!=s.end()){
+                val++;
+            }
+        }
+        return val;
+    }
+};
